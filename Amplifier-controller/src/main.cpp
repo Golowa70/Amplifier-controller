ed79add52422c9057fbf5378ca355c7dd1b209c7
YyuriD <yurid.fkg@gmail.com>
2024-05-14 22:44:08 +0300

    commit

--- Amplifier-controller/src/main.cpp
+++ Amplifier-controller/src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "defines.h"
+#include "variables.h"
+#include "init_functions.h"
+
+void fnInputsUpdate(void);
+void fnOutputsUpdate(Data& data);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+}
+//******************************************************************
+
+void fnInputsUpdate(void) {
+  main_data.thermostat1_state = digitalRead(THERMOSTAT_1);
+  main_data.thermostat2_state = digitalRead(THERMOSTAT_2);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void fnOutputsUpdate(Data& data) {
+  digitalWrite(POWER_RELAY_OUT, main_data.power_relay_state);
+  digitalWrite(SPEAKERS_A_OUT, main_data.sp_A_out_state);
+  digitalWrite(SPEAKERS_B_OUT, main_data.sp_B_out_state);
+  digitalWrite(DIRECT_INPUT_RELAY_OUT, main_data.direct_relay_state);
 }
+
