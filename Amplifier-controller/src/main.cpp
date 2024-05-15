167eae6a68aea548e81910f4c9d74edfb0a46d33
YyuriD <yurid.fkg@gmail.com>
2024-05-15 19:23:32 +0300

    commit

--- Amplifier-controller/src/main.cpp
+++ Amplifier-controller/src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
 
 #include "defines.h"
 #include "variables.h"
 #include "init_functions.h"
 
-void fnInputsUpdate(void);
-void fnOutputsUpdate(Data& data);
+
+void TaskOutputsUpdate(void* pvParameters);
+void TaskInputsUpdate(void* pvParameters);
+
 
 void setup() {
+
+  xTaskCreate(
+    TaskInputsUpdate, "InputsUpdate" // A name just for humans
+    ,
+    192 // This stack size can be checked & adjusted by reading the Stack Highwater //128
+    ,
+    NULL, 3 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,
+    NULL);
+
+  xTaskCreate(TaskOutputsUpdate, "InputsUpdate", 128, NULL, 3, NULL);
+
 
 }
 
@@ -16,15 +32,20 @@ void loop() {
 }
 //******************************************************************
 
-void fnInputsUpdate(void) {
-  main_data.thermostat1_state = digitalRead(THERMOSTAT_1);
-  main_data.thermostat2_state = digitalRead(THERMOSTAT_2);
+void TaskInputsUpdate(void* pvParameters __attribute__((unused))) {
+  for (;;) {
+    main_data.thermostat1_state = digitalRead(THERMOSTAT_1);
+    main_data.thermostat2_state = digitalRead(THERMOSTAT_2);
+  }
 }
 
-void fnOutputsUpdate(Data& data) {
-  digitalWrite(POWER_RELAY_OUT, main_data.power_relay_state);
-  digitalWrite(SPEAKERS_A_OUT, main_data.sp_A_out_state);
-  digitalWrite(SPEAKERS_B_OUT, main_data.sp_B_out_state);
-  digitalWrite(DIRECT_INPUT_RELAY_OUT, main_data.direct_relay_state);
+
+void TaskOutputsUpdate(void* pvParameters __attribute__((unused))) {
+  for (;;) {
+    digitalWrite(POWER_RELAY_OUT, main_data.power_relay_state);
+    digitalWrite(SPEAKERS_A_OUT, main_data.sp_A_out_state);
+    digitalWrite(SPEAKERS_B_OUT, main_data.sp_B_out_state);
+    digitalWrite(DIRECT_INPUT_RELAY_OUT, main_data.direct_relay_state);
+  }
 }
 
